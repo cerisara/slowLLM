717f830f8c176617130daccdbd4750d97b74e665
xtof <cerisara@loria.fr>
2023-02-12 13:00:30 +0100

    ok

--- code/slowBloom.py
+++ code/slowBloom.py
@@ -5,12 +5,27 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, BloomConfig
 from accelerate import init_empty_weights
 from typing import Optional, Tuple, Union
 import torch
+import gc
 
 wd = "/home/xtof/sda5/bloom/"
 wd = "/mnt/dos/xtof/"
-singletonBlock = None
 curlayer = 0
 allblocks = []
+
+pnames = (
+        'input_layernorm.weight',
+        'input_layernorm.bias',
+        'self_attention.query_key_value.weight',
+        'self_attention.query_key_value.bias',
+        'self_attention.dense.weight',
+        'self_attention.dense.bias',
+        'post_attention_layernorm.weight',
+        'post_attention_layernorm.bias',
+        'mlp.dense_h_to_4h.weight',
+        'mlp.dense_h_to_4h.bias',
+        'mlp.dense_4h_to_h.weight',
+        'mlp.dense_4h_to_h.bias',
+        )
 
 class MyBloomBlock(transformers.models.bloom.modeling_bloom.BloomBlock):
     def __init__(self, config):
@@ -18,99 +33,42 @@ class MyBloomBlock(transformers.models.bloom.modeling_bloom.BloomBlock):
         self.memAllocated = False
         # there's one such block created per layer
         # when the first one is created, it'll be the only one with actual parameters
-        global singletonBlock, curlayer, allblocks
+        global curlayer, allblocks
         self.numLayer = curlayer
         curlayer += 1
-        if singletonBlock==None:
-            singletonBlock = self
-            self.memAllocated = False
-        else:
-            self.memAllocated = True
         allblocks.append(self)
+        self.emptyParms = [p for p in self.parameters()]
+
+    def assignParms(self,pname,v):
+        if pname==pnames[0]: self.input_layernorm.weight = v
+        if pname==pnames[1]: self.input_layernorm.bias = v
+        if pname==pnames[2]: self.self_attention.query_key_value.weight = v
+        if pname==pnames[3]: self.self_attention.query_key_value.bias = v
+        if pname==pnames[4]: self.self_attention.dense.weight = v
+        if pname==pnames[5]: self.self_attention.dense.bias = v
+        if pname==pnames[6]: self.post_attention_layernorm.weight = v
+        if pname==pnames[7]: self.post_attention_layernorm.bias = v
+        if pname==pnames[8]: self.mlp.dense_h_to_4h.weight = v
+        if pname==pnames[9]: self.mlp.dense_h_to_4h.bias = v
+        if pname==pnames[10]: self.mlp.dense_4h_to_h.weight = v
+        if pname==pnames[11]: self.mlp.dense_4h_to_h.bias = v
+
+    def emptyLayer(self):
+        for i in range(len(pnames)): self.assignParms(pnames[i],self.emptyParms[i])
+        gc.collect()
 
     def loadLayer(self,l):
-        if not self.memAllocated:
-            print("allocate RAM for one layer")
-            nparms = sum(p.numel() for _,p in self.named_parameters())
-            print("parmsize for one layer",4.*nparms/1000000000.,"GB")
-
-            p=self.input_layernorm.weight
-            self.input_layernorm.weight = torch.nn.Parameter(torch.zeros_like(p,device="cpu",dtype=torch.float32,requires_grad=False))
-            p=self.input_layernorm.bias
-            self.input_layernorm.bias = torch.nn.Parameter(torch.zeros_like(p,device="cpu",dtype=torch.float32,requires_grad=False))
-            p=self.self_attention.query_key_value.weight
-            self.self_attention.query_key_value.weight = torch.nn.Parameter(torch.zeros_like(p,device="cpu",dtype=torch.float32,requires_grad=False))
-            p=self.self_attention.query_key_value.bias
-            self.self_attention.query_key_value.bias = torch.nn.Parameter(torch.zeros_like(p,device="cpu",dtype=torch.float32,requires_grad=False))
-            p=self.self_attention.dense.weight
-            self.self_attention.dense.weight = torch.nn.Parameter(torch.zeros_like(p,device="cpu",dtype=torch.float32,requires_grad=False))
-            p=self.self_attention.dense.bias
-            self.self_attention.dense.bias = torch.nn.Parameter(torch.zeros_like(p,device="cpu",dtype=torch.float32,requires_grad=False))
-            p=self.post_attention_layernorm.weight
-            self.post_attention_layernorm.weight = torch.nn.Parameter(torch.zeros_like(p,device="cpu",dtype=torch.float32,requires_grad=False))
-            p=self.post_attention_layernorm.bias
-            self.post_attention_layernorm.bias = torch.nn.Parameter(torch.zeros_like(p,device="cpu",dtype=torch.float32,requires_grad=False))
-            p=self.mlp.dense_h_to_4h.weight
-            self.mlp.dense_h_to_4h.weight = torch.nn.Parameter(torch.zeros_like(p,device="cpu",dtype=torch.float32,requires_grad=False))
-            p=self.mlp.dense_h_to_4h.bias
-            self.mlp.dense_h_to_4h.bias = torch.nn.Parameter(torch.zeros_like(p,device="cpu",dtype=torch.float32,requires_grad=False))
-            p=self.mlp.dense_4h_to_h.weight
-            self.mlp.dense_4h_to_h.weight = torch.nn.Parameter(torch.zeros_like(p,device="cpu",dtype=torch.float32,requires_grad=False))
-            p=self.mlp.dense_4h_to_h.bias
-            self.mlp.dense_4h_to_h.bias = torch.nn.Parameter(torch.zeros_like(p,device="cpu",dtype=torch.float32,requires_grad=False))
-            for p in self.parameters(): p.requires_grad=False
-
-            for layer in allblocks[1:]:
-                # use the same parms tensor than the first block ! Just overwrite the values inside...
-                layer.input_layernorm.weight = singletonBlock.input_layernorm.weight
-                layer.input_layernorm.bias = singletonBlock.input_layernorm.bias
-                layer.self_attention.query_key_value.weight = singletonBlock.self_attention.query_key_value.weight
-                layer.self_attention.query_key_value.bias = singletonBlock.self_attention.query_key_value.bias
-                layer.self_attention.dense.weight = singletonBlock.self_attention.dense.weight
-                layer.self_attention.dense.bias = singletonBlock.self_attention.dense.bias
-                layer.post_attention_layernorm.weight = singletonBlock.post_attention_layernorm.weight
-                layer.post_attention_layernorm.bias = singletonBlock.post_attention_layernorm.bias
-                layer.mlp.dense_h_to_4h.weight = singletonBlock.mlp.dense_h_to_4h.weight
-                layer.mlp.dense_h_to_4h.bias = singletonBlock.mlp.dense_h_to_4h.bias
-                layer.mlp.dense_4h_to_h.weight = singletonBlock.mlp.dense_4h_to_h.weight
-                layer.mlp.dense_4h_to_h.bias = singletonBlock.mlp.dense_4h_to_h.bias
-
         print("load weights from disk")
         t0 = time.time()
         f = "0000"+str(l+2) if l<8 else "000"+str(l+2)
-        prebloc = [None]*70
         parms = torch.load(wd+"pytorch_model_"+f+"-of-00072.bin")
-        prebloc[0] = parms['h.'+str(l)+'.input_layernorm.weight'].to(dtype=torch.float32)
-        prebloc[1] = parms['h.'+str(l)+'.input_layernorm.bias'].to(dtype=torch.float32)
-        prebloc[2] = parms['h.'+str(l)+'.self_attention.query_key_value.weight'].to(dtype=torch.float32)
-        prebloc[3] = parms['h.'+str(l)+'.self_attention.query_key_value.bias'].to(dtype=torch.float32)
-        prebloc[4] = parms['h.'+str(l)+'.self_attention.dense.weight'].to(dtype=torch.float32)
-        prebloc[5] = parms['h.'+str(l)+'.self_attention.dense.bias'].to(dtype=torch.float32)
-        prebloc[6] = parms['h.'+str(l)+'.post_attention_layernorm.weight'].to(dtype=torch.float32)
-        prebloc[7] = parms['h.'+str(l)+'.post_attention_layernorm.bias'].to(dtype=torch.float32)
-        prebloc[8] = parms['h.'+str(l)+'.mlp.dense_h_to_4h.weight'].to(dtype=torch.float32)
-        prebloc[9] = parms['h.'+str(l)+'.mlp.dense_h_to_4h.bias'].to(dtype=torch.float32)
-        prebloc[10] = parms['h.'+str(l)+'.mlp.dense_4h_to_h.weight'].to(dtype=torch.float32)
-        prebloc[11] = parms['h.'+str(l)+'.mlp.dense_4h_to_h.bias'].to(dtype=torch.float32)
-        del parms
-        for i in range(12): prebloc[i] = torch.nn.Parameter(prebloc[i],requires_grad=False)
+        for i in range(len(pnames)):
+            prebloc = parms['h.'+str(l)+'.'+pnames[i]].to(dtype=torch.float32)
+            del parms['h.'+str(l)+'.'+pnames[i]]
+            prebloc = torch.nn.Parameter(prebloc,requires_grad=False)
+            self.assignParms(pnames[i],prebloc)
         t1 = time.time()
         print("preloaded OK",l,t1-t0)
-
-        print("copy parms in block",l)
-        singletonBlock.input_layernorm.weight.copy_(prebloc[0])
-        singletonBlock.input_layernorm.bias.copy_(prebloc[1])
-        singletonBlock.self_attention.query_key_value.weight.copy_(prebloc[2])
-        singletonBlock.self_attention.query_key_value.bias.copy_(prebloc[3])
-        singletonBlock.self_attention.dense.weight.copy_(prebloc[4])
-        singletonBlock.self_attention.dense.bias.copy_(prebloc[5])
-        singletonBlock.post_attention_layernorm.weight.copy_(prebloc[6])
-        singletonBlock.post_attention_layernorm.bias.copy_(prebloc[7])
-        singletonBlock.mlp.dense_h_to_4h.weight.copy_(prebloc[8])
-        singletonBlock.mlp.dense_h_to_4h.bias.copy_(prebloc[9])
-        singletonBlock.mlp.dense_4h_to_h.weight.copy_(prebloc[10])
-        singletonBlock.mlp.dense_4h_to_h.bias.copy_(prebloc[11])
-        del prebloc 
 
     def forward(
         self,
@@ -146,5 +104,5 @@ t1 = time.time()
 t1-=t0
 print("empty model loaded",t1,"RAM",resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
 
-singletonBlock.loadLayer(49)
+allblocks[0].loadLayer(49)
 
