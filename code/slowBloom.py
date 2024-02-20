87122ca33e6123d7bb6cea6f07a8ccca102fe623
xtof <cerisara@loria.fr>
2024-02-20 20:58:08 +0100

    ok

--- code/slowBloom.py
+++ code/slowBloom.py
@@ -563,7 +563,7 @@ def magnitude_pruning(b):
         for n,p in b.named_parameters():
             p.requires_grad=False
             if len(p.shape)==2:
-                if p.dtype=="meta": continue
+                if p.device=="meta": continue
                 print(n,p.shape)
                 # prune row-wise as suggested in Wanda paper
                 metric = p.abs()
